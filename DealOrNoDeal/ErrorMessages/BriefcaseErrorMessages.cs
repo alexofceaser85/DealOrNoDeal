2049c802cd411b42dcc9eff7cfc6ff993ed87ae4
Alex <contact@alexdecesare.com>
2021-08-21 15:37:19 -0400

    Added the Briefcase class and the BriefcaseErrorMessages class
    Added the data class for the Briefcase and the error messages class.

---
+++ DealOrNoDeal/ErrorMessages/BriefcaseErrorMessages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealOrNoDeal.ErrorMessages
+{
+    /// <summary>
+    /// Holds the error messages for the Briefcase class.
+    /// </summary>
+    public static class BriefcaseErrorMessages
+    {
+        /// <summary>
+        /// The brief case identifier cannot be zero or less
+        /// </summary>
+        public const string BriefCaseIdCannotBeZeroOrLess = "The brief case Id cannot be less than zero";
+        /// <summary>
+        /// The dollar amount cannot be less than zero
+        /// </summary>
+        public const string DollarAmountCannotBeLessThanZero = "The dollar amount cannot be less than zero";
+    }
+}
---
+++ DealOrNoDeal/Model/Briefcase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace DealOrNoDeal.Model
+{
+    /// <summary>
+    /// Holds the data for each briefcase
+    /// </summary>
+    public class Briefcase
+    {
+        /// <summary>
+        /// The briefcase identifier.
+        /// </summary>
+        public int BriefcaseId { get; set; }
+
+        /// <summary>
+        /// The dollar amount
+        /// </summary>
+        public int DollarAmount { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Briefcase"/> class.
+        /// </summary>
+        ///
+        /// <precondition>
+        /// briefcaseId &>= 0
+        /// dollarAmount &>= 0 
+        /// </precondition>
+        ///
+        /// <postcondition>
+        /// this.BriefcaseId == briefcaseId
+        /// this.DollarAmount == dollarAmount
+        /// </postcondition>
+        /// 
+        /// <param name="briefcaseId">The briefcase identifier.</param>
+        /// <param name="dollarAmount">The dollar amount.</param>
+        public Briefcase(int briefcaseId, int dollarAmount)
+        {
+            if (briefcaseId < 0)
+            {
+                throw new ArgumentException(ErrorMessages.BriefcaseErrorMessages.BriefCaseIdCannotBeZeroOrLess);
+            }
+
+            if (dollarAmount < 0)
+            {
+                throw new ArgumentException(ErrorMessages.BriefcaseErrorMessages.DollarAmountCannotBeLessThanZero);
+            }
+
+            this.BriefcaseId = briefcaseId;
+            this.DollarAmount = dollarAmount;
+        }
+    }
+}
