90b52f752ebdb83bb0bb16dd1d9bf308ab9db0ff
Alex <contact@alexdecesare.com>
2021-08-22 14:19:34 -0400

    Refactored the populate briefcases method
    Split the populate briefcases method into two methods, a private method to get the random indexes and the public facing populate briefcases method.

--- DealOrNoDealByAlexDeCesare/DealOrNoDeal/Model/GameManager.cs
+++ DealOrNoDealByAlexDeCesare/DealOrNoDeal/Model/GameManager.cs
@@ -9,7 +9,7 @@ namespace DealOrNoDeal.Model
     {
         #region Constructors
 
-        private readonly int[] briefCaseDollarAmounts =
+        private readonly IList<int> briefCaseDollarAmounts = new List<int>
         {
             0, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000,
             200000, 300000, 400000, 500000, 750000, 1000000
@@ -78,13 +78,36 @@ namespace DealOrNoDeal.Model
         public GameManager()
         {
             theBriefcases = new List<Briefcase>();
-            PopulateBriefcases();
+            PopulateBriefcases(GetRandomIndexesToAccessDollarValues(briefCaseDollarAmounts.Count, 0,
+                briefCaseDollarAmounts.Count));
             CurrentRound = InitialCurrentRound;
             CasesLeftForCurrentRound = InitialCasesLeftForCurrentRound;
             PlayerSelectedStartingCase = InitialPlayerSelectedStartingCase;
             BankerCurrentOffer = InitialBankerCurrentOffer;
             MinimumBankerOffer = InitialBankerMinimumOffer;
             MaximumBankerOffer = InitialBankerMaximumOffer;
+        }
+
+        /// <summary>
+        /// Populates the briefcases
+        /// </summary>
+        ///
+        /// <precondition>
+        /// none
+        /// </precondition>
+        ///
+        /// <postcondition>
+        /// this.theBriefcases.Count == this.indexesOfDollarValuesToPopulate.Length
+        /// </postcondition>
+        /// <param name="indexesOfDollarValuesToPopulate">The index of the array element from the briefCaseDollarAmounts to associate with the briefcase</param>
+        public void PopulateBriefcases(IList<int> indexesOfDollarValuesToPopulate)
+        {
+            for (var counter = 0; counter < indexesOfDollarValuesToPopulate.Count; counter++)
+            {
+                var currentDollarAmountIndex = indexesOfDollarValuesToPopulate[counter];
+                var newBriefcase = new Briefcase(counter, briefCaseDollarAmounts[currentDollarAmountIndex]);
+                theBriefcases.Add(newBriefcase);
+            }
         }
 
         #endregion
@@ -158,29 +181,63 @@ namespace DealOrNoDeal.Model
             // TODO Complete this method according to its specification
         }
 
+        /// <summary>
+        /// Returns the string representation of the game manager
+        /// </summary>
+        ///
+        /// <precondition>
+        /// none
+        /// </precondition>
+        /// <postcondition>
+        /// none
+        /// </postcondition>
+        /// 
+        /// <returns>The string representation of the game manager</returns>
+        public override string ToString()
+        {
+            var gameManagerString = $"A game manager with the following information:\n"
+                                       + $"current round: {CurrentRound}\n"
+                                       + $"cases left for current round: {CasesLeftForCurrentRound}\n"
+                                       + $"player selected starting case: {PlayerSelectedStartingCase}\n"
+                                       + $"banker current offer: {BankerCurrentOffer}\n"
+                                       + $"minimum banker offer: {MinimumBankerOffer}\n"
+                                       + $"maximum banker offer: {MaximumBankerOffer}\n"
+                                       + $"---And the following briefcases---";
+
+            foreach (var briefcase in this.theBriefcases)
+            {
+                gameManagerString +=
+                    $"A briefcase with an ID: {briefcase.BriefcaseId} and dollar amount {briefcase.DollarAmount}\n";
+            }
+
+            return gameManagerString;
+        }
+
         private Briefcase GetBriefcaseById(int briefcaseIdToGet)
         {
             return theBriefcases.SingleOrDefault(p => p.BriefcaseId == briefcaseIdToGet);
         }
 
-        private void PopulateBriefcases()
+        private static IList<int> GetRandomIndexesToAccessDollarValues(int numberOfIndexesToGenerate,
+            int minimumValueToGenerate, int maximumValueToGenerate)
         {
             var randomNumberGenerator = new Random();
-            var numberOfBriefcases = briefCaseDollarAmounts.Length;
+            IList<int> randomIndexes = new List<int>();
             IList<int> previousIndexes = new List<int>();
 
-            for (var counter = 0; counter < numberOfBriefcases; counter++)
+            for (var counter = 0; counter < numberOfIndexesToGenerate; counter++)
             {
                 int currentIndex;
                 do
                 {
-                    currentIndex = randomNumberGenerator.Next(0, numberOfBriefcases);
+                    currentIndex = randomNumberGenerator.Next(minimumValueToGenerate, maximumValueToGenerate);
                 } while (previousIndexes.Contains(currentIndex));
 
+                randomIndexes.Add(currentIndex);
                 previousIndexes.Add(currentIndex);
-                var newBriefcase = new Briefcase(counter, briefCaseDollarAmounts[currentIndex]);
-                theBriefcases.Add(newBriefcase);
             }
+
+            return randomIndexes;
         }
 
         #endregion
